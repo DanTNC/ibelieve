b4b8b3326b39e84d5030eb53458145bad544a81f
dantnc <fad11204@yahoo.com.tw>
2021-09-12 00:15:17 +0800

    [Feature] set up callback listener of Spotify OAuth api

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send(req.query.token)
 })
 
+app.get('/callback', (req, res) => {
+    res.send(`${req.query.code}:${req.query.state}`)
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
