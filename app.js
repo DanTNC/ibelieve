39103dc9e0a0a15aaa47e0bcc78e7633bcb150f8
dantnc <fad11204@yahoo.com.tw>
2021-09-12 01:07:27 +0800

    [Feature] set up Spotify OAuth routes

--- app.js
+++ app.js
@@ -2,12 +2,41 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+redirect_uri = 'https://ibelieve.herokuapp.com/callback'
+
 app.get('/', (req, res) => {
-  res.send(req.query.token)
+  res.send("Home")
 })
 
+app.get('/login', function(req, res) {
+var scopes = 'playlist-read-private';
+res.redirect('https://accounts.spotify.com/authorize' +
+    '?response_type=code' +
+    '&client_id=' + process.env.ibelieve_client_id +
+    (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
+    '&redirect_uri=' + encodeURIComponent(redirect_uri));
+});
+
 app.get('/callback', (req, res) => {
-    res.send(`${req.query.code}:${req.query.state}`)
+  axios.post(
+    'https://accounts.spotify.com/api/token',
+    {
+      code: req.query.code,
+      grant_type: 'authorization_code',
+      redirect_uri: encodeURIComponent(redirect_uri)
+    },
+    {
+      headers: {
+        'Authorization': 'Authorization: Basic ' + process.env.ibelieve_auth,
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+    } 
+  ).then((response) => {
+    console.log(response)
+  }).catch((error) => {
+    console.log(error)
+  })
+  res.send(`${req.query.code}:${req.query.state}`)
 })
 
 app.listen(port, () => {
