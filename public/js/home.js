7d449694d51dc606b72a6390514dbdb4b6adcef7
dantnc <fad11204@yahoo.com.tw>
2021-09-12 17:19:52 +0800

    [Fix] add login button click handler

--- public/js/home.js
+++ public/js/home.js
@@ -5,6 +5,10 @@ $(() => {
         $("#uid").val(uid)
     }
 
+    $("#login").click(() => {
+        window.location = window.location.protocol + "//" + window.location.hostname + ":" + window.location.port + "/login?uid=" + $("#uid").val()
+    })
+
     $("#signup").click(() => {
         window.location = window.location.protocol + "//" + window.location.hostname + ":" + window.location.port + "/signup"
     })
